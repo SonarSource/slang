be2321312684c676ac9801cb47f45cda0f3aa4a6
Christophe Zürn <christophe.zurn@sonarsource.com>
2018-06-05 09:31:49 +0200

    Add metadata to kotlin AST

--- slang-kotlin/src/test/java/com/sonarsource/slang/kotlin/KotlinParserTest.java
+++ slang-kotlin/src/test/java/com/sonarsource/slang/kotlin/KotlinParserTest.java
@@ -21,8 +21,10 @@ package com.sonarsource.slang.kotlin;
 
 import com.sonarsource.slang.api.BinaryExpressionTree;
 import com.sonarsource.slang.api.BinaryExpressionTree.Operator;
+import com.sonarsource.slang.api.TextRange;
 import com.sonarsource.slang.api.Tree;
 import com.sonarsource.slang.visitors.TreeContext;
+import com.sonarsource.slang.visitors.TreePrinter;
 import com.sonarsource.slang.visitors.TreeVisitor;
 import java.io.IOException;
 import java.util.LinkedList;
@@ -58,10 +60,27 @@ public class KotlinParserTest {
     assertHasXNodesOfOperatorType(allNodes, Operator.EQUAL_TO, 1);
   }
 
+  @Test
+  public void testRange() {
+    Tree tree = KotlinParser.fromString("fun function1() =\n(1 + 2 * 3 * 4 / 5 - 6)\n == 32;");
+    assertRange(tree, 1, 0, 3, 7);
+    BinaryExpressionTree topMostBinaryExpression = ((BinaryExpressionTree) tree.children().get(2).children().get(1).children().get(0));
+    assertRange(topMostBinaryExpression, 2, 1, 2, 22);
+    assertRange(topMostBinaryExpression.leftOperand(), 2, 1, 2, 18);
+  }
+
   private static void assertHasXNodesOfOperatorType(List<Tree> nodes, Operator operator, int expectedNumber) {
     assertThat(nodes)
       .filteredOn(node -> node instanceof BinaryExpressionTree && ((BinaryExpressionTree) node).operator() == operator)
       .hasSize(expectedNumber);
+  }
+
+  private static void assertRange(Tree tree, int startLine, int startLineOffset, int endLine, int endLineOffset) {
+    TextRange range = tree.metaData().textRange();
+    assertThat(range.start().line()).isEqualTo(startLine);
+    assertThat(range.start().lineOffset()).isEqualTo(startLineOffset);
+    assertThat(range.end().line()).isEqualTo(endLine);
+    assertThat(range.end().lineOffset()).isEqualTo(endLineOffset);
   }
 
 }
