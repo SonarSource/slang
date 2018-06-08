850544619b7a0b8c69b61efeafb5effcad233cdd
Pierre-Yves Nicolas <pierre-yves.nicolas@sonarsource.com>
2018-06-08 16:35:21 +0200

    Improve coverage

---
+++ slang-api/src/test/java/com/sonarsource/slang/impl/MatchCaseTreeImplTest.java
@@ -0,0 +1,43 @@
+/*
+ * SonarSource SLang
+ * Copyright (C) 2009-2018 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package com.sonarsource.slang.impl;
+
+import com.sonarsource.slang.api.MatchCaseTree;
+import com.sonarsource.slang.api.Tree;
+import com.sonarsource.slang.api.TreeMetaData;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MatchCaseTreeImplTest {
+
+  @Test
+  public void test() {
+    TreeMetaData meta = null;
+    Tree expression = new LiteralTreeImpl(meta, "42");
+    Tree body = new IdentifierTreeImpl(meta, "x");
+    MatchCaseTree tree = new MatchCaseTreeImpl(meta, expression, body);
+    assertThat(tree.children()).containsExactly(expression, body);
+    assertThat(tree.expression()).isEqualTo(expression);
+    assertThat(tree.body()).isEqualTo(body);
+
+    assertThat(new MatchCaseTreeImpl(meta, null, body).children()).containsExactly(body);
+  }
+}
