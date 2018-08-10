e737f0aa83232e6eba75e7a37c597aae664c5e52
Christophe Zürn <36889251+christophe-zurn-sonarsource@users.noreply.github.com>
2018-08-10 14:44:45 +0200

    SONARSLANG-129 SonarRuby: fix parser issue related to literals (#171)

--- sonar-ruby-plugin/src/main/resources/whitequark_parser_init.rb
+++ sonar-ruby-plugin/src/main/resources/whitequark_parser_init.rb
@@ -1,18 +1,31 @@
 require 'parser/ruby25'
 
-# opt-in to most recent AST format (used for Backwards compatibility when breaking changes are introduced in AST format)
-# In order not to break backward compatibility, when breaking changes are introduced in a newer version of the parser AST, these new
-# features have to be manually enabled. Here we enable the latest/current features at time of development.
-Parser::Builders::Default.emit_lambda   = true
-Parser::Builders::Default.emit_procarg0 = true
-Parser::Builders::Default.emit_encoding = true
-Parser::Builders::Default.emit_index    = true
+# Custom builder in order to avoid throwing an error when literals contain an escape sequences incompatible with UTF-8
+# See https://github.com/whitequark/parser/issues/283
+# Inspired from https://github.com/eapache/starscope/pull/166
+class Builder < Parser::Builders::Default
+
+  # opt-in to most recent AST format (used for Backwards compatibility when breaking changes are introduced in AST format)
+  # In order not to break backward compatibility, when breaking changes are introduced in a newer version of the parser AST, these new
+  # features have to be manually enabled. Here we enable the latest/current features at time of development.
+  @emit_lambda   = true
+  @emit_procarg0 = true
+  @emit_encoding = true
+  @emit_index    = true
+
+  def string_value(token)
+    value(token)
+  end
+end
 
 # In order to retrieve AST, comments, and tokens, we need to use the 'tokenize' method of the ruby Parser object.
 # However, the 'tokenize' method takes directly a Buffer object as parameter. Here, we map the string content to the Buffer object the
 # same way it is done in the 'Parser::Base.parse' and 'Parser::Base.setup_source_buffer' methods.
 def parse_with_tokens(content, filename='(string)')
-  parser = Parser::Ruby25.default_parser
+  parser = Parser::Ruby25.new(Builder.new)
+  parser.diagnostics.all_errors_are_fatal = true
+  parser.diagnostics.ignore_warnings = true
+
   content = content.dup.force_encoding(parser.default_encoding)
   source_buffer = Parser::Source::Buffer.new(filename, 1)
   source_buffer.source = content
