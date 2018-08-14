e5c1c447d585368a4602a6cd5909674e62832a5b
Elena <elena.vilchik@sonarsource.com>
2018-08-14 11:29:51 +0200

    Convert to Slang unary, binary, parenthesized expressions and boolean literals

--- sonar-ruby-plugin/src/test/java/org/sonarsource/ruby/converter/visitor/LiteralVisitorTest.java
+++ sonar-ruby-plugin/src/test/java/org/sonarsource/ruby/converter/visitor/LiteralVisitorTest.java
@@ -17,34 +17,31 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-package org.sonarsource.ruby.converter;
+package org.sonarsource.ruby.converter.visitor;
 
 
 import org.junit.Test;
-import org.sonarsource.slang.api.TopLevelTree;
-import org.sonarsource.slang.api.Tree;
+import org.sonarsource.ruby.converter.AbstractRubyConverterTest;
 
-import static java.util.Collections.emptyList;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.sonarsource.slang.testing.TreeAssert.assertTree;
+import static org.sonarsource.slang.testing.TreesAssert.assertTrees;
 
-public class RubyVisitorTest extends AbstractRubyConverterTest {
+public class LiteralVisitorTest extends AbstractRubyConverterTest {
 
   @Test
-  public void top_level_tree() {
-    assertTree(converter.parse(("true\nfalse"))).isInstanceOf(TopLevelTree.class);
-    assertTree(converter.parse(("true\r\nfalse"))).isInstanceOf(TopLevelTree.class);
+  public void boolean_literal() {
+    assertTree(rubyStatement("true")).isLiteral("true");
+    assertTree(rubyStatement("false")).isLiteral("false");
   }
 
   @Test
-  public void parse_with_missing_node() {
-    Tree tree = converter.parse("def is_root?\nend"); // method has null argument list
-    assertThat(tree).isNotNull();
-  }
+  public void int_literals() {
+    assertTrees(rubyStatements("2; 512; 4\n2431323"))
+      .isEquivalentTo(slangStatements("2; 512; 4; 2431323;"));
+    assertTree(rubyStatement("2")).isLiteral("2");
 
-  @Test
-  public void singletons() {
-    assertTree(rubyStatement("nil")).isEquivalentTo(nativeTree(nativeKind("nil"), emptyList()));
+    // literal bigger than Long.MAX_VALUE are returned as BigInteger by JRuby
+    assertTree(rubyStatement("10000000000000000000")).isLiteral("10000000000000000000");
   }
 
 }
