8180f8bb35d33aadf47ff63c65db782d99886c3f
vilchik-elena <elena.vilchik@sonarsource.com>
2018-08-14 15:06:35 +0200

    SONARSLANG-139 Enable in Ruby rule S1940 (BooleanInversionCheck)

--- slang-checks/src/main/java/org/sonarsource/slang/checks/CheckList.java
+++ slang-checks/src/main/java/org/sonarsource/slang/checks/CheckList.java
@@ -34,7 +34,6 @@ public class CheckList {
 
   private static final Class[] RUBY_CHECK_BLACK_LIST = {
     AllBranchesIdenticalCheck.class,
-    BooleanInversionCheck.class,
     BooleanLiteralCheck.class,
     CodeAfterJumpCheck.class,
     DuplicateBranchCheck.class,
