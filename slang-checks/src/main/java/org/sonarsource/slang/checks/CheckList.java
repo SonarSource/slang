8c919c648ec8f02d8cb94091d270cd95b0ef401d
vilchik-elena <elena.vilchik@sonarsource.com>
2018-08-15 14:17:40 +0200

    SONARSLANG-133 Enable in Ruby rule S1151 (MatchCaseTooBigCheck)

--- slang-checks/src/main/java/org/sonarsource/slang/checks/CheckList.java
+++ slang-checks/src/main/java/org/sonarsource/slang/checks/CheckList.java
@@ -47,7 +47,6 @@ public class CheckList {
     IdenticalBinaryOperandCheck.class,
     IdenticalConditionsCheck.class,
     IfConditionalAlwaysTrueOrFalseCheck.class,
-    MatchCaseTooBigCheck.class,
     OneStatementPerLineCheck.class,
     RedundantParenthesesCheck.class,
     SelfAssignmentCheck.class,
