a08847cb549670495241db7f18f2b17ff4d356f1
Alban Auzeill <alban.auzeill@sonarsource.com>
2018-10-08 17:28:28 +0200

    SONARSLANG-235 [Scala] Enable Rule: S1125 Boolean literals should not be redundant

--- slang-checks/src/main/java/org/sonarsource/slang/checks/CheckList.java
+++ slang-checks/src/main/java/org/sonarsource/slang/checks/CheckList.java
@@ -45,7 +45,6 @@ public class CheckList {
   };
 
   private static final Class[] SCALA_CHECK_BLACK_LIST = {
-    BooleanLiteralCheck.class,
     CodeAfterJumpCheck.class,
     MatchWithoutElseCheck.class,
     OctalValuesCheck.class,
