f3eadd4c1d5471573257326baa42d3ec873cbc19
Tom <thomas.verin@sonarsource.com>
2018-11-30 09:51:25 +0100

    Use correct env vars for slang build

--- travis.sh
+++ travis.sh
@@ -5,14 +5,14 @@ if [ "${TRAVIS_REPO_SLUG}" == "SonarSource/slang" ];then
   echo "Building slang"
   
   ./gradlew --no-daemon --console plain \
-    -DbuildNumber=$BUILD_NUMBER \
+    -DbuildNumber=$TRAVIS_BUILD_NUMBER \
     build sonarqube \
     -Dsonar.host.url=$SONAR_HOST_URL \
     -Dsonar.login=$SONAR_TOKEN \
     -Dsonar.projectVersion=$INITIAL_VERSION \
-    -Dsonar.analysis.buildNumber=$BUILD_NUMBER \
-    -Dsonar.analysis.pipeline=$BUILD_NUMBER \
-    -Dsonar.analysis.sha1=$GIT_COMMIT \
+    -Dsonar.analysis.buildNumber=$TRAVIS_BUILD_NUMBER \
+    -Dsonar.analysis.pipeline=$TRAVIS_BUILD_NUMBER \
+    -Dsonar.analysis.sha1=$TRAVIS_COMMIT \
     -Dsonar.analysis.repository=$TRAVIS_REPO_SLUG \
     -Dsonar.organization=sonarsource
 
