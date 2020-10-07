d324b3f6c97a341c0a6251895c82755eaa8ba2e4
Amélie Renard <44666826+amelie-renard-sonarsource@users.noreply.github.com>
2020-10-07 16:17:31 +0200

    SONARSLANG-446 Update S126 to not raise issues on cases where there is return/break/... (#130)

--- slang-checks/src/main/java/org/sonarsource/slang/checks/ElseIfWithoutElseCheck.java
+++ slang-checks/src/main/java/org/sonarsource/slang/checks/ElseIfWithoutElseCheck.java
@@ -24,9 +24,16 @@ import org.sonarsource.slang.api.IfTree;
 import org.sonarsource.slang.api.TextRange;
 import org.sonarsource.slang.api.Token;
 import org.sonarsource.slang.api.Tree;
+import org.sonarsource.slang.api.BlockTree;
+import org.sonarsource.slang.api.JumpTree;
+import org.sonarsource.slang.api.ReturnTree;
+import org.sonarsource.slang.api.ThrowTree;
+import org.sonarsource.slang.checks.api.CheckContext;
 import org.sonarsource.slang.checks.api.InitContext;
 import org.sonarsource.slang.checks.api.SlangCheck;
 import org.sonarsource.slang.impl.TextRangeImpl;
+
+import java.util.List;
 
 @Rule(key = "S126")
 public class ElseIfWithoutElseCheck implements SlangCheck {
@@ -36,20 +43,58 @@ public class ElseIfWithoutElseCheck implements SlangCheck {
   @Override
   public void initialize(InitContext init) {
     init.register(IfTree.class, (ctx, ifTree) -> {
-      Tree elseBranch = ifTree.elseBranch();
-      if (elseBranch instanceof IfTree) {
-        IfTree nestedIfTree = (IfTree) elseBranch;
-        if (nestedIfTree.elseBranch() == null) {
-          Token elseToken = ifTree.elseKeyword();
-          Token ifToken = nestedIfTree.ifKeyword();
-          TextRange textRange = new TextRangeImpl(
-            elseToken.textRange().start(),
-            ifToken.textRange().end()
-          );
-          ctx.reportIssue(textRange, MESSAGE);
-        }
+      if (ifTree.elseBranch() == null || !isTopLevelIf(ctx, ifTree)) {
+        return;
       }
-    });
 
+      IfTree prevTree = ifTree;
+      boolean endsWithReturn = endsWithReturnBreakOrThrow(ifTree);
+      while (ifTree.elseBranch() instanceof IfTree) {
+        prevTree = ifTree;
+        ifTree = (IfTree)(ifTree.elseBranch());
+        endsWithReturn = endsWithReturn && endsWithReturnBreakOrThrow(ifTree);
+      }
+
+      // We raise an issue if
+      //   - at least one branch does not finish with return/break/throw
+      //   - no "else" is defined
+      if (!endsWithReturn && ifTree.elseBranch() == null) {
+        Token elseToken = prevTree.elseKeyword();
+        Token ifToken = ifTree.ifKeyword();
+        TextRange textRange = new TextRangeImpl(
+                elseToken.textRange().start(),
+                ifToken.textRange().end()
+        );
+        ctx.reportIssue(textRange, MESSAGE);
+      }
+
+    });
+  }
+
+  private static boolean isTopLevelIf(CheckContext ctx, IfTree ifTree) {
+    Tree firstAncestor = ctx.ancestors().getFirst();
+    if (firstAncestor instanceof IfTree) {
+      // if ifTree is different from the else branch of firstAncestor, it means that ifTree is a statement inside
+      // firstAncestor and so ifTree is the top level "if"
+      return ((IfTree) firstAncestor).elseBranch() != ifTree;
+    }
+    return true;
+  }
+
+  private static boolean endsWithReturnBreakOrThrow(IfTree ifTree) {
+    Tree thenBranch = ifTree.thenBranch();
+    if (thenBranch instanceof BlockTree) {
+      List<Tree> statements = ((BlockTree) thenBranch).statementOrExpressions();
+      if (!statements.isEmpty()) {
+        Tree lastStmt = statements.get(statements.size() - 1);
+        return isReturnBreakOrThrow(lastStmt);
+      }
+    }
+    // Curly braces can be omitted when there is only one statement inside the "if"
+    return isReturnBreakOrThrow(thenBranch);
+  }
+
+  private static boolean isReturnBreakOrThrow(Tree tree) {
+    return tree instanceof JumpTree || tree instanceof ReturnTree || tree instanceof ThrowTree;
   }
 }
