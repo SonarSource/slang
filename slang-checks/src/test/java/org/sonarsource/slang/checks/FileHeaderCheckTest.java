56497b79d2913064d65788bf391bccc769417681
Samir M <samir.marini@sonarsource.com>
2025-11-14 14:21:54 +0100

    SC-35440 scripted updated of SonarSource SA to SonarSource Sàrl

--- slang-checks/src/test/java/org/sonarsource/slang/checks/FileHeaderCheckTest.java
+++ slang-checks/src/test/java/org/sonarsource/slang/checks/FileHeaderCheckTest.java
@@ -1,10 +1,10 @@
 /*
  * SonarSource SLang
- * Copyright (C) 2018-2025 SonarSource SA
+ * Copyright (C) 2018-2025 SonarSource Sàrl
  * mailto:info AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
- * modify it under the terms of the Sonar Source-Available License Version 1, as published by SonarSource SA.
+ * modify it under the terms of the Sonar Source-Available License Version 1, as published by SonarSource Sàrl.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -39,7 +39,7 @@ class FileHeaderCheckTest {
     check.headerFormat = """
       /*
        * SonarSource SLang
-       * Copyright (C) 1999-2001 SonarSource SA
+       * Copyright (C) 1999-2001 SonarSource Sàrl
        * mailto:info AT sonarsource DOT com
        */""";
     Verifier.verifyNoIssue("fileheader/Multiline.slang", check);
--- slang-checks/src/test/resources/org/sonarsource/slang/checks/fileheader/Multiline.slang
+++ slang-checks/src/test/resources/org/sonarsource/slang/checks/fileheader/Multiline.slang
@@ -1,6 +1,6 @@
 /*
  * SonarSource SLang
- * Copyright (C) 1999-2001 SonarSource SA
+ * Copyright (C) 1999-2001 SonarSource Sàrl
  * mailto:info AT sonarsource DOT com
  */
 // comment
